43938f9ff5e440132725bc3210558773c3200a56
john_wisneski <john.j.wisneski@gmail.com>
2020-12-22 23:40:24 -0500

    commit tree 622bf7eb89678e7eb87a9a04ddb3862fc75ee55c

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,42 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css("div.student-card a").each do |student|
+        student_profile_link = student.attr("href")
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
+    end
+    students
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+    profile_page = Nokogiri::HTML(open(profile_url))
+    profile = {}
+    social_icon_links = profile_page.css(".social-icon-container a")
+
+    social_icon_links.each do |link|
+      link_text = link.attr("href")
+      
+      if link_text.include?("linkedin")
+        profile[:linkedin] = link_text
+      elsif link_text.include?("twitter")
+        profile[:twitter] = link_text
+      elsif link_text.include?("github")
+        profile[:github] = link_text
+      else
+        profile[:blog] = link_text
+      end
+    end
+
+    profile[:profile_quote] = profile_page.css("div.profile-quote").text
+    profile[:bio] = profile_page.css("div.description-holder p").text
+    profile
   end
 
 end
